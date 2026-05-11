7dbdce0f2eb7d65ab8f19210f28335dc351da2e3
Musubidachi <fabriziom19@gmail.com>
2026-05-11 00:14:18 -0400

    Remove generated builder from validation verdict entity

--- lib-persistence/src/main/java/dev/reddragon/persistence/entity/ValidationVerdictEntity.java
+++ lib-persistence/src/main/java/dev/reddragon/persistence/entity/ValidationVerdictEntity.java
@@ -8,7 +8,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -17,7 +16,6 @@ import java.time.Instant;
 @Entity
 @Table(name = "validation_verdict")
 @Getter
-@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class ValidationVerdictEntity {
